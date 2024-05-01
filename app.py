0cf8fd46ce8e8c86f6bf91c7eeff95cbcbb138ff
khushi-rajput04 <khushirajput147@gmail.com>
2024-05-02 01:42:01 +0530

    added vercel.json and wsgi.py file

--- app.py
+++ app.py
@@ -52,5 +52,3 @@ def transcribe_microphone(): # Input from microphone
     return render_template('index.html', transcript=transcript, status_message=status_message)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
